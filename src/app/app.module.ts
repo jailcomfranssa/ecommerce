bca789f4c7e120c134ad3e1eb021ddf061ad59e6
jailcomfranssa <jail_son_0@hotmail.com>
2022-08-09 13:44:29 -0300

    criando os componentes header, main e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templete/header/header.component';
+import { MainComponent } from './templete/main/main.component';
+import { FooterComponent } from './templete/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
